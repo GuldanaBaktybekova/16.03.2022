0bec1bbf243b15166444b83ab7dd39496f86a093
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-16 14:28:44 +0600

    Added const

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+
+function App() {
+  const studentStyle = {
+    boxShadow: "0 0 10px black",
+    padding: "10px",
+    
+  }
+  return (
+    <div className="App">
+     <div style={}>
+       <h2>John Doe</h2>
+       <div>Score: 4.5/5</div>
+       <div>Homework: 4/5</div>
+     </div>
+    
+       <div>
+       <h2>Elon Mask</h2>
+       <div>Score: 3/5</div>
+       <div>Homework: 5/5</div>
+     </div>
+    
+    <div>
+    <h2>Donald Trump</h2>
+    <div>Score: 2/5</div>
+    <div>Homework: 1/5</div>
+  </div>
+  </div>
+  );
+}
+
+export default App;
