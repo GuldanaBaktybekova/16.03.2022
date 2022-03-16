9eaf9adb423b2c7ec231bdb3dc973c88e746769a
Guldana Baktybekova <guldanabaktybekova10@gmail.com>
2022-03-16 14:35:39 +0600

    Added style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-
+import "./App.css"
 
 function App() {
-  const studentStyle = {
+  const studentStyles = {
     boxShadow: "0 0 10px black",
     padding: "10px",
-    
-  }
+    margin: "10px",
+  };
   return (
     <div className="App">
-     <div style={}>
+     <div style={studentStyles}>
        <h2>John Doe</h2>
        <div>Score: 4.5/5</div>
        <div>Homework: 4/5</div>
      </div>
     
-       <div>
+       <div style={studentStyles} >
        <h2>Elon Mask</h2>
        <div>Score: 3/5</div>
        <div>Homework: 5/5</div>
      </div>
     
-    <div>
+    <div style={studentStyles}>
     <h2>Donald Trump</h2>
     <div>Score: 2/5</div>
     <div>Homework: 1/5</div>
